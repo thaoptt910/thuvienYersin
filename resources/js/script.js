ffe5e07872fe9adb89cc54cdf5d99ed6072ba526
Thao Pham <thaoptt910@gmail.com>
2017-11-12 16:32:42 +0700

    check phone and email

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,51 @@
 $(function() {
+
+  function checkEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+    }
+
+    function checkPhoneNumber(){
+      var flag = false; // sai sdt
+      //trim bo khoang trang
+      //val lay gia tri cua input
+      var phone = $('#input_phone').val().trim(); // ID của trường Số điện thoại
+      phone = phone.replace('(+84)', '0');
+      phone = phone.replace('+84', '0');
+      phone = phone.replace('0084', '0');
+      phone = phone.replace(/ /g, '');
+      if (phone != '') { // co nhap
+          var firstNumber = phone.substring(0, 2); //0900999 => 09
+          if ((firstNumber == '09') && phone.length == 10) { // 09 la 10 so
+              if (phone.match(/^\d{10}/)) { //thuat toan
+                  flag = true; // dung sdt
+              }
+          } else if (firstNumber == '01' && phone.length == 11) { // 01 la 11 so
+              if (phone.match(/^\d{11}/)) {
+                  flag = true; // dung sdt
+              }
+          }
+      }
+      return flag;
+    }
+
+    $('#form_submit').click(function(e) { // ID của Form
+      $('.error').html(''); //clear error
+      if (!checkPhoneNumber()) { //TH sai sdt
+          //$('#input_phone').parents('div.form-group').addClass('has-error'); // ID của trường Số điện thoại
+          $('#input_phone').next('.error').append('<span style="color:red">Sai số điện thoại</span>');
+          // $('.error').text('Sai số điện thoại');
+          $('#input_phone').focus(); // ID của trường Số điện thoại
+          e.preventDefault(); // ngăn không click nhieu lan
+      }
+      var $email = $('#input_email').val().trim();
+      if(!checkEmail($email)){
+        $('#input_email').next('.error').append('<span style="color:red">Sai email</span>');
+        $('#input_email').focus(); // ID của trường Số điện thoại
+        e.preventDefault(); // ngăn không click nhieu lan
+      }
+  });
+
     /*menu*/
     $('#main-menu').smartmenus({
       subMenusSubOffsetX: 1,
@@ -22,16 +69,16 @@ $(function() {
         $('#main-nav').css('display','none');
       }
 
- 
+
 
     /*content 1*/
       var clickEvent = false;
   $('#content1').carousel({
-    interval:   4000  
+    interval:   4000
   }).on('click', '.list-group li', function() {
       clickEvent = true;
       $('.list-group li').removeClass('active');
-      $(this).addClass('active');   
+      $(this).addClass('active');
   }).on('slid.bs.carousel', function(e) {
     if(!clickEvent) {
       var count = $('.list-group').children().length -1;
@@ -39,7 +86,7 @@ $(function() {
       current.removeClass('active').next().addClass('active');
       var id = parseInt(current.data('slide-to'));
       if(count == id) {
-        $('.list-group li').first().addClass('active'); 
+        $('.list-group li').first().addClass('active');
       }
     }
     clickEvent = false;
@@ -54,11 +101,11 @@ $(function() {
     /*content 1*/
     // var clickEvent = false;
     // $('#banner').carousel({
-    //   interval:   4000  
+    //   interval:   4000
     // }).on('click', '.list-group li', function() {
     //     clickEvent = true;
     //     $('.list-group li').removeClass('active');
-    //     $(this).addClass('active');   
+    //     $(this).addClass('active');
     // }).on('slid.bs.carousel', function(e) {
     //   if(!clickEvent) {
     //     var count = $('.list-group').children().length -1;
@@ -66,7 +113,7 @@ $(function() {
     //     current.removeClass('active').next().addClass('active');
     //     var id = parseInt(current.data('slide-to'));
     //     if(count == id) {
-    //       $('.list-group li').first().addClass('active'); 
+    //       $('.list-group li').first().addClass('active');
     //     }
     //   }
     //   clickEvent = false;
@@ -92,7 +139,7 @@ $(function() {
           },
           700:{
               items:2,
-          } , 
+          } ,
           750:{
               items:3,
           },
@@ -117,7 +164,7 @@ $(function() {
           },
           700:{
               items:2,
-          } , 
+          } ,
           750:{
               items:3,
           },
@@ -131,7 +178,7 @@ $(function() {
     })
     /* end owl carousel*/
     $("#owl-demo").owlCarousel({
- 
+
       navigation : true, // Show next and prev buttons
       slideSpeed : 300,
       paginationSpeed : 400,
@@ -141,24 +188,24 @@ $(function() {
       autoplay:true,
       items : 1,
       // "singleItem:true" is a shortcut for:
-      // items : 1, 
+      // items : 1,
       // itemsDesktop : false,
       // itemsDesktopSmall : false,
       // itemsTablet: false,
       // itemsMobile : false
- 
+
   });
-   
+
 
     /*test*/
 
   var clickEvent = false;
   $('#myCarousel').carousel({
-    interval:   4000  
+    interval:   4000
   }).on('click', '.list-group li', function() {
       clickEvent = true;
       $('.list-group li').removeClass('active');
-      $(this).addClass('active');   
+      $(this).addClass('active');
   }).on('slid.bs.carousel', function(e) {
     if(!clickEvent) {
       var count = $('.list-group').children().length -1;
@@ -166,7 +213,7 @@ $(function() {
       current.removeClass('active').next().addClass('active');
       var id = parseInt(current.data('slide-to'));
       if(count == id) {
-        $('.list-group li').first().addClass('active'); 
+        $('.list-group li').first().addClass('active');
       }
     }
     clickEvent = false;
